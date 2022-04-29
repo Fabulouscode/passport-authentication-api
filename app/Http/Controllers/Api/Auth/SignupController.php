89c755da092216baff1a74acc833d173a6eaa1da
Fabulouscode <ulaghaondo@gmail.com>
2022-04-29 16:07:06 +0100

    User Signup, route, controller and Model modified

---
+++ app/Http/Controllers/Api/Auth/SignupController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use App\Http\Resources\SignupResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class SignupController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone_number' => $request->phone_number,
+        ]);
+
+        if(!$user) {
+            return response([
+                'status' => 'fail',
+                'code' => 400,
+                'message' => 'Unable to create account.Try again!'
+            ])->setStatusCode(400);
+        }
+
+        return response([
+            'status' => 'success',
+            'code' => 201,
+            'message' => 'Account created Successfully. Check email to activate your account.',
+            'data' => new SignupResource($user),
+        ])->setStatusCode(201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\SignupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['middleware' => ['cors', 'json.response']], function () {
- 
+Route::prefix('v1')->group(static function(){
+    Route::prefix('users')->name('user.')->group(static function(){
+        Route::post('/signUp', [SignupController::class, 'signup'])->name('signup');
+    });
 });
